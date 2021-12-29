74517826590d6cb678a13ec0029509524b473af7
Isabel <natyferaudo23@gmail.com>
2021-12-28 21:22:45 -0300

    constructor, Getters and setters

--- ToDoListBackend/src/main/java/com/IsabelFeraudo/model/Folder.java
+++ ToDoListBackend/src/main/java/com/IsabelFeraudo/model/Folder.java
@@ -1,11 +1,34 @@
 package com.IsabelFeraudo.model;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table (name="folder")
 
 public class Folder {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int idFolder;
 	private String name;
+	public Folder(String name) {
+		super();
+		this.name = name;
+	}
+	public int getIdFolder() {
+		return idFolder;
+	}
+	public void setIdFolder(int idFolder) {
+		this.idFolder = idFolder;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
--- ToDoListBackend/src/main/java/com/IsabelFeraudo/model/Item.java
+++ ToDoListBackend/src/main/java/com/IsabelFeraudo/model/Item.java
@@ -1,13 +1,47 @@
 package com.IsabelFeraudo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table (name="item")
 public class Item {
-	
+
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
 private int idItem;
 private String description;
+@ManyToOne
+@JoinColumn(name="idFolder")
 private Folder folder;
+public Item(String description, Folder folder) {
+	super();
+	this.description = description;
+	this.folder = folder;
+}
+public int getIdItem() {
+	return idItem;
+}
+public void setIdItem(int idItem) {
+	this.idItem = idItem;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public Folder getFolder() {
+	return folder;
+}
+public void setFolder(Folder folder) {
+	this.folder = folder;
+}
+
+
 }
