e61922ffd0aae713be4d3ce04d69037e5a8088b9
Isabel <natyferaudo23@gmail.com>
2021-12-29 21:49:15 -0300

    services  created

--- ToDoListFrontEnd/src/app/app.component.ts
+++ ToDoListFrontEnd/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ItemsService } from './services/items/items.service';
+import { FoldersService } from './services/folders/folders.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ToDoListFrontEnd';
+  itemForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public itemService: ItemsService
+
+  ){}
+  
+ngOnInit(): void {
+  this.itemForm=this.fb.group({
+    idItem:['',Validators.required],
+    description:['',Validators.required],
+  })
+    
+}
 }
