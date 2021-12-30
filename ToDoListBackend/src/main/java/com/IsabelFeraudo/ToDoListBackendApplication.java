3f17bc570cbfdbd7621a2ec085d7aad9217e7835
Isabel <natyferaudo23@gmail.com>
2021-12-30 12:12:30 -0300

    CORS problem solved

--- ToDoListBackend/src/main/java/com/IsabelFeraudo/ToDoListBackendApplication.java
+++ ToDoListBackend/src/main/java/com/IsabelFeraudo/ToDoListBackendApplication.java
@@ -2,6 +2,9 @@ package com.IsabelFeraudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ToDoListBackendApplication {
@@ -9,5 +12,17 @@ public class ToDoListBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListBackendApplication.class, args);
 	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 
 }
+
+
